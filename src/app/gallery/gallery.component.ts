273875735305d398ab9e54e8d870ae8e951d6900
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2024-08-17 20:07:57 -0400

    Little more formatting

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,35 +1,42 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    FormsModule
   ],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
   Title = "Prefab Gallery";
+  Search = "";
 
   GalleryImages = [
-    {imgSrc: 'assets/gallery/sample1.jpg', title: "Image1"},
-    {imgSrc: 'assets/gallery/sample2.jpg', title: "Image2"},
-    {imgSrc: 'assets/gallery/sample3.jpg', title: "Image3"},
-    {imgSrc: 'assets/gallery/sample4.jpg', title: "Image4"},
-    {imgSrc: 'assets/gallery/sample5.jpg', title: "Image5"},
-    {imgSrc: 'assets/gallery/sample6.jpg', title: "Image6"},
-    {imgSrc: 'assets/gallery/sample7.jpg', title: "Image7"},
-    {imgSrc: 'assets/gallery/sample9.jpg', title: "Image9"},
-    {imgSrc: 'assets/gallery/sample10.jpg', title: "Image10"},
-    {imgSrc: 'assets/gallery/sample11.jpg', title: "Image11"},
-    {imgSrc: 'assets/gallery/sample8.jpg', title: "Image8"},
-    {imgSrc: 'assets/gallery/sample12.png', title: "Image12"},
+    {imgSrc: 'assets/gallery/sample1.jpg', title: "Image1", tags: "white, games"},
+    {imgSrc: 'assets/gallery/sample2.jpg', title: "Image2", tags: "white, games"},
+    {imgSrc: 'assets/gallery/sample3.jpg', title: "Image3", tags: "white, games"},
+    {imgSrc: 'assets/gallery/sample4.jpg', title: "Image4", tags: "white, games"},
+    {imgSrc: 'assets/gallery/sample5.jpg', title: "Image5", tags: "blue, games"},
+    {imgSrc: 'assets/gallery/sample6.jpg', title: "Image6", tags: "blue, games"},
+    {imgSrc: 'assets/gallery/sample7.jpg', title: "Image7", tags: "blue, sports"},
+    {imgSrc: 'assets/gallery/sample9.jpg', title: "Image9", tags: "blue, sports"},
+    {imgSrc: 'assets/gallery/sample10.jpg', title: "Image10", tags: "blue, sports"},
+    {imgSrc: 'assets/gallery/sample11.jpg', title: "Image11", tags: "white, sports"},
+    {imgSrc: 'assets/gallery/sample8.jpg', title: "Image8", tags: "white, sports"},
+    {imgSrc: 'assets/gallery/sample12.png', title: "Image12", tags: "white, sports"},
   ];
 
   trackByItemTitle(index: number, item: { title: string }): string {
     return item.title;
+  }
+
+  filteredSelection() {
+    return this.GalleryImages.filter((image) => image.tags.includes(this.Search))
   }
 }
