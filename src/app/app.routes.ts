702a17fae898dbf3a24881cdf314f29c3c878252
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2024-08-21 01:46:36 -0400

    Early phone formatting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import {ContactUsComponent} from './contact-us/contact-us.component'
 import {AboutUsComponent} from './about-us/about-us.component'
 import {GalleryComponent} from './gallery/gallery.component'
 import {GalleryDoorDetailComponent} from "./gallery-door-detail/gallery-door-detail.component";
+import {BaseDoorComponent} from "./base-door/base-door.component";
 
 export const routes: Routes = [
   { path: 'door-designer', component: DoorDesignerComponent },
@@ -12,4 +13,5 @@ export const routes: Routes = [
   { path: 'contact-us', component: ContactUsComponent },
   { path: 'gallery', component: GalleryComponent },
   { path: 'gallery-door-detail/:id', component: GalleryDoorDetailComponent },
+  { path: 'base-door', component: BaseDoorComponent }
 ];
